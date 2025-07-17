e29ba13e461acd24f86a56b63e1bff324943d8ff
T Nikhil Chowdary <tnikhilchowdary@gmail.com>
2025-07-17 12:40:59 -0500

    Updated feature/fixed bug/added new section

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navbar from './componnets/Navbar';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
+import Men from "./pages/Men";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/men" element={<Men />} />
         </Routes>
       </Router>
     </div>
--- src/pages/Men.js
+++ src/pages/Men.js
@@ -1,1 +1,34 @@
 import React, {useEffect, useState} from "react";
+import { data } from "react-router-dom";
+import ProductCard from "../componnets/ProductCard";
+import "./Men.css";
+
+const Men = () => {
+    const [menProducts, setMenProducts] = useState([]);
+    useEffect(() => {
+        const fetchMenProducts = async () => {
+            const urls = [
+                'https://dummyjson.com/products/category/mens-shirts',
+                'https://dummyjson.com/products/category/mens-shoes',
+                'https://dummyjson.com/products/category/mens-watches',
+            ];
+
+            const responses = await Promise.all(
+                urls.map(url => fetch(url).then(res => res.json()))
+            );
+
+            const combinedProducts = responses.flatMap(data => data.products);
+            setMenProducts(combinedProducts)
+        };
+        fetchMenProducts();
+    }, [])
+    return(
+        <div className="men-products-container" >
+            {menProducts.map(product => (
+                <ProductCard key={product.id} product={product}/>
+            ))}
+        </div>
+    )
+}
+
+export default Men;
